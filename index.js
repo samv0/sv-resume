e37b39b9f1013179e389a66f22d53dffe7b3b54d
Sam Vinh <samvinh0x@gmail.com>
2023-02-09 10:37:46 -0800

    Release version 1.0 of resume

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  let content = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+
+  document.getElementById('count').innerHTML = 1234;
+
+})
